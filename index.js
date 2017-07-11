9bb8844d5724ac33460833c82c3c70630d87381b
zhoukk <izhoukk@gmail.com>
2017-07-11 16:26:28 +0800

    remove usb, serialport, network adapter

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 /**
  * Adapters
  */
-exports.USB     = require('./adapter/usb');
-exports.Serial  = require('./adapter/serial');
-exports.Network = require('./adapter/network');
 exports.Console = require('./adapter/console');
 /**
  * Printer Supports
